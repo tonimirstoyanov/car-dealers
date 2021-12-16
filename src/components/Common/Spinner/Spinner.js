1d4a32d261d77fa36ff2501c5e6a536ea3b060bb
tonimirstoyanov <93668598+tonimirstoyanov@users.noreply.github.com>
2021-12-16 19:20:10 +0200

    add loading spinner

---
+++ src/components/Common/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import styles from './Spinner.module.css'
+
+const Spinner = () => {
+
+    return (
+        <div className={styles['lds-spinner']}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default Spinner

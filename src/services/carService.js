4251107d0669741366534a7c7ec4613037fa39be
tonimirstoyanov <93668598+tonimirstoyanov@users.noreply.github.com>
2021-12-16 19:17:21 +0200

    Add edit, like, dislike request

--- src/services/carService.js
+++ src/services/carService.js
@@ -9,7 +9,7 @@ export const lastAdded = () => {
 
 export const getOne = (id) => {
     return fetch(`http://localhost:3030/car/${id}/details`)
-    .then(response => response.json())
+        .then(response => response.json())
 }
 
 export const create = async (data, token) => {
@@ -39,7 +39,58 @@ export const deleteOne = async (carId, token) => {
         }
     })
     let jsonResult = await result.json();
-    
+
+    if (result.ok) {
+        return jsonResult
+    } else {
+        throw jsonResult.message
+    }
+}
+
+export const updateOne = async (carId, data, token) => {
+    let result = await fetch(`http://localhost:3030/car/${carId}/edit`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'auth_token': token
+        },
+        body: JSON.stringify(data)
+    })
+    let jsonResult = await result.json();
+    console.log(jsonResult)
+    if (result.ok) {
+        return jsonResult
+    } else {
+        throw jsonResult.message
+    }
+}
+
+export const like = async (carId, data, token) => {
+    let result = await fetch(`http://localhost:3030/car/${carId}/likes`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'auth_token': token
+        },
+        body: JSON.stringify(data)
+    })
+    let jsonResult = await result.json();
+    if (result.ok) {
+        return jsonResult
+    } else {
+        throw jsonResult.message
+    }
+}
+export const dislike = async (carId, data, token) => {
+    let result = await fetch(`http://localhost:3030/car/${carId}/dislikes`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'auth_token': token
+        },
+        body: JSON.stringify(data)
+    })
+    let jsonResult = await result.json();
     if (result.ok) {
         return jsonResult
     } else {

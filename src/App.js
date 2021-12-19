ebe1d55ce4dcfd103f25310cda83eff4659cc158
tonimirstoyanov <93668598+tonimirstoyanov@users.noreply.github.com>
2021-12-19 19:29:00 +0200

    add notification context

--- src/App.js
+++ src/App.js
@@ -9,28 +9,33 @@ import Register from './components/Register/Register.js';
 import Edit from './components/Edit/Edit.js';
 import Create from './components/Create/Create.js';
 import Details from './components/Details/Details.js';
+import PageNotFound from './components/PageNotFound/PageNotFound.js';
 import { Logout } from './components/Logout/Logout.js';
-import { AuthProvider} from './context/authContext.js';
+import { AuthProvider } from './context/authContext.js';
+import { NotificationProvider } from './context/notificationContext.js'
 
 function App() {
 
 
     return (
         <AuthProvider>
-            <div className="App">
-                <Header />
+            <NotificationProvider>
+                <div className="App">
+                    <Header />
 
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/car/catalog" component={Catalog} />
-                    <Route path="/car/create" component={Create} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/logout" component={Logout} />
-                    <Route path="/car/:carId/details" component={Details} />
-                    <Route path="/car/:carId/edit" component={Edit} />
-                </Switch>
-            </div>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/car/catalog" component={Catalog} />
+                        <Route path="/car/create" component={Create} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/register" component={Register} />
+                        <Route path="/logout" component={Logout} />
+                        <Route path="/car/:carId/details" component={Details} />
+                        <Route path="/car/:carId/edit" component={Edit} />
+                        <Route path="*" component={PageNotFound} />
+                    </Switch>
+                </div>
+            </NotificationProvider>
         </AuthProvider>
     );
 }
